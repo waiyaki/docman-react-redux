c24209645d06633effbb8dbbd0f3d21be12e82b6
James Muturi <james.muturi@andela.com>
2016-04-21 23:36:14 +0300

    Refactor routes to handle auth middleware better

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,36 @@
 (function () {
   'use strict';
 
-  module.exports = function (app) {
-    // Users Routes.
-    require('./users/users')(app);
+  var usersController = require('../controllers').usersController;
+  var customMiddleware = require('../middleware');
 
-    // Root path.
+  module.exports = function (app) {
+    // Return a default response for the root url.
     app.get('/', function (req, res) {
       res.json({message: 'Welcome to DocMan'});
     });
+
+    // Create a user
+    app.post('/users', function (req, res, next) {
+      return customMiddleware.validatePost(req, res, next, {
+        required_fields: ['username', 'email', 'password']
+      });
+    }, usersController.create);
+
+    // Login a user
+    app.post('/users/login', function (req, res, next) {
+      return customMiddleware.validatePost(req, res, next, {
+        required_fields: ['username', 'password']
+      });
+    }, usersController.login);
+
+    /**
+     * Beyond this point, only authenticated users should access the API.
+     * Unauthenticated users can only either login or register.
+     */
+    app.use(customMiddleware.authenticate);
+
+    // Users Routes.
+    require('./users/users')(app);
   };
 })();
--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -1,32 +1,8 @@
 (function () {
   'use strict';
 
-  var usersController = require('../../controllers').usersController;
-  var customMiddleware = require('../../middleware');
+  // var usersController = require('../../controllers').usersController;
+  // var customMiddleware = require('../../middleware');
 
-  module.exports = function (app) {
-    /**
-     * Login a user.
-     */
-    app.post('/users/login', function (req, res, next) {
-      return customMiddleware.validatePost(req, res, next, {
-        required_fields: ['username', 'password']
-      });
-    }, usersController.login);
-
-    /**
-     * Create a new user
-     */
-    app.post('/users', function (req, res, next) {
-      return customMiddleware.validatePost(req, res, next, {
-        required_fields: ['username', 'email', 'password']
-      });
-    }, usersController.create);
-
-    /**
-     * Beyond this point, only authenticated users should access the API.
-     * Unauthenticated users can only either login or register.
-     */
-    app.use(customMiddleware.authenticate);
-  };
+  module.exports = function (app) {};
 })();
