b78b610d9fe9573d0956e459f1b62260c9854053
James Muturi <james.muturi@andela.com>
2016-05-26 15:02:18 +0300

    Show toasts after updates (#11)
    Show toast messages after successful or erroneous
    updates/logins/signups.
    Add css to reset the browser specific stylesheets and achieve
    uniformity.

--- client/src/containers/Main/MainContainer.jsx
+++ client/src/containers/Main/MainContainer.jsx
@@ -4,12 +4,15 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 // eslint-disable-next-line
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import SnackBarContainer from '../Utils/SnackBarContainer';
+
 export default class MainContainer extends React.Component {
   render () {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div className='main-application'>
           {this.props.children}
+          <SnackBarContainer />
         </div>
       </MuiThemeProvider>
     );
