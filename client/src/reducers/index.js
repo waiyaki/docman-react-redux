6caa6b67279ef3a23ed0838b7029deecfb837861
James Muturi <james.muturi@andela.com>
2016-05-29 22:09:50 +0300

    [#120141477] Add functionality to fetch roles

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import AuthReducer from './AuthReducer';
 import UserDetailsReducer from './UserDetailsReducer';
 import UtilsReducer from './UtilsReducer';
 import DocumentsReducer from './DocumentsReducer';
+import RolesReducer from './RolesReducer';
 
 const RootReducer = combineReducers({
   auth: AuthReducer,
   docs: DocumentsReducer,
+  roles: RolesReducer,
   userDetails: UserDetailsReducer,
   utils: UtilsReducer
 });
