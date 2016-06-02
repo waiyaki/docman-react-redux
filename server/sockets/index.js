f63e44f48abcbc5a37346245fd353bacfa38e527
James Muturi <james.muturi@andela.com>
2016-06-02 10:10:32 +0300

    Remove logging statements from serverside socketio

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -5,13 +5,7 @@
     var io = require('socket.io')(server, {path: '/docman/rtc'});
 
     io.on('connection', function (socket) {
-      socket.emit('welcome', {welcome: 'welcome to DocMan'});
-      socket.on('thankyou', function (data) {
-        console.log('Received thank you: ', data);
-      });
-
       socket.on('role:join', function (role) {
-        console.log('Joining: ', role);
         socket.join(role);
       });
 
