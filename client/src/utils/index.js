e1fc8044344770c526c834c293e82382b133744f
James Muturi <james.muturi@andela.com>
2016-06-04 14:31:06 +0300

    [#120822087] Test AuthActions
    Abstract setting of auth token in localStorage into a separate
    function to allow for easier mocking.

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,11 @@
 export function getToken () {
   return window.localStorage.getItem('token');
 }
+
+export function setAuthToken (token) {
+  window.localStorage.setItem('token', token);
+}
+
+export function removeAuthToken () {
+  window.localStorage.removeItem('token');
+}
