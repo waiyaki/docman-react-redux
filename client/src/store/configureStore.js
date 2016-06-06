247cdf43a64c6085647cb0ec7d438581a4b7c202
James Muturi <james.muturi@andela.com>
2016-06-07 02:05:25 +0300

    Prepare deployment to Heroku

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,25 +1,14 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 
 import RootReducer from '../reducers';
-
-const loggerMiddleware = createLogger({
-  collapsed: true,
-  duration: true,
-  timestamp: true,
-  stateTransformer: function (state) {
-    return state.toJS();
-  }
-});
 
 export default function configureStore (initialState) {
   return createStore(
     RootReducer,
     initialState,
     applyMiddleware(
-      thunkMiddleware, // Enable us to dispatch functions.
-      loggerMiddleware // Log all actions.
+      thunkMiddleware // Enable us to dispatch functions
     )
   );
 }
