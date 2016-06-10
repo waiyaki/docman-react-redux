73d10dc1dbd061db8f864b9b17bbefd82efab004
James Muturi <james.muturi@andela.com>
2016-06-11 01:28:38 +0300

    [#120822139] Add tests for the UserSidebar
    Add tests for the UserSidebarUpdate functionality
    Stub `console.error` in order to silence warnings about missing
    proptypes when components are mounted in tests.

---
+++ test/client/components/UserSidebar/UserSidebarTests.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {shallowWithContext} from '../utils';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import UserSidebar from '../../../../client/src/components/UserSidebar/UserSidebar';
+
+describe('UserSidebar', () => {
+  const props = {
+    handleToggleShowUpdate: () => {},
+    userDetails: {
+      user: {}
+    }
+  };
+
+  it('renders a user profile card', () => {
+    const wrapper = shallowWithContext(<UserSidebar {...props} />);
+    expect(wrapper.find('Card')).to.be.length(1);
+  });
+
+  it('renders a card header with a user avatar', () => {
+    const wrapper = shallowWithContext(<UserSidebar {...props} />);
+    const CardHeader = wrapper.find('CardHeader');
+    expect(CardHeader.props().avatar).to.match(/https:\/\/www.gravatar.com/);
+  });
+
+  it('renders an edit button when showing own profile', () => {
+    const wrapper = shallowWithContext(<UserSidebar {...props} />);
+    wrapper.setProps({
+      isOwnProfile: true
+    });
+    const IconMenu = wrapper.find('IconMenu');
+    expect(IconMenu).to.have.length(1);
+    const MenuItem = wrapper.find('MenuItem');
+    expect(MenuItem.props().primaryText).to.equal('Edit Profile');
+  });
+
+  it('toggles between showing profile and updating profile', () => {
+    const handleToggleShowUpdate = sinon.spy();
+    const wrapper = shallowWithContext(<UserSidebar {...props} />);
+    wrapper.setProps({
+      isOwnProfile: true,
+      handleToggleShowUpdate: handleToggleShowUpdate
+    });
+    const MenuItem = wrapper.find('MenuItem');
+    MenuItem.simulate('touchTap');
+    expect(handleToggleShowUpdate.calledOnce).to.be.true;
+  });
+});
---
+++ test/client/components/UserSidebar/UserSidebarUpdateTests.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import {shallowWithContext} from '../utils';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import UserSideBarUpdate from '../../../../client/src/components/UserSideBar/UserProfileUpdate';
+
+describe('UserSideBarUpdate', () => {
+  const props = {
+    handleFieldUpdate: () => {},
+    handleProfileUpdate: () => {},
+    handleToggleShowUpdate: () => {},
+    handleValidateFieldOnBlur: () => {},
+    userDetails: {
+      updatedUser: {},
+      user: {
+        username: 'test',
+        role: {}
+      },
+      validations: {}
+    }
+  };
+
+  it('renders a user profile update card', () => {
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    expect(wrapper.find('CardTitle').props().title)
+      .to.equal('test - Profile Update');
+  });
+
+  it('renders username, email, firstName, lastName, password' +
+     'and confirmPassword text fields', () => {
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    const textFields = wrapper.find('TextField');
+
+    const expectedFields = [
+      'username', 'email', 'firstName', 'lastName',
+      'password', 'confirmPassword'
+    ];
+
+    textFields.nodes.forEach(node => {
+      expect(!!~(expectedFields.indexOf(node.props.name))).to.be.true;
+    });
+  });
+
+  it('renders validation errors', () => {
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    wrapper.setProps({
+      userDetails: {
+        updatedUser: {},
+        user: {
+          username: 'test',
+          role: {}
+        },
+        validations: {
+          email: 'This field is required.'
+        }
+      }
+    });
+
+    const validationError = wrapper.find('ValidationError').first();
+    expect(validationError.props().error)
+      .to.equal('This field is required.');
+  });
+
+  it('calls onChange handler when a field is updated', () => {
+    const handleFieldUpdate = sinon.spy();
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    wrapper.setProps({
+      handleFieldUpdate: handleFieldUpdate
+    });
+    const textField = wrapper.find('TextField').last();
+    textField.simulate('change');
+    expect(handleFieldUpdate.calledOnce).to.be.true;
+  });
+
+  it('validates on blur', () => {
+    const handleValidateFieldOnBlur = sinon.spy();
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    wrapper.setProps({
+      handleValidateFieldOnBlur: handleValidateFieldOnBlur
+    });
+    const textField = wrapper.find('TextField').last();
+    textField.simulate('blur');
+    expect(handleValidateFieldOnBlur.calledOnce).to.be.true;
+  });
+
+  it('has a disabled Update button if the data is invalid', () => {
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    const updateButton = wrapper.find('RaisedButton').first();
+    expect(updateButton.props().disabled).to.be.true;
+
+    wrapper.setProps({
+      userDetails: {
+        updatedUser: {},
+        user: {
+          username: 'test',
+          role: {}
+        },
+        validations: {
+          isValid: true
+        }
+      }
+    });
+
+    const updateButton2 = wrapper.find('RaisedButton').first();
+    expect(updateButton2.props().disabled).to.be.false;
+  });
+
+  it('calls handleProfileUpdate when the update button is clicked', () => {
+    const handleProfileUpdate = sinon.spy();
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    wrapper.setProps({
+      handleProfileUpdate: handleProfileUpdate,
+      userDetails: {
+        updatedUser: {},
+        user: {
+          username: 'test',
+          role: {}
+        },
+        validations: {
+          isValid: true
+        }
+      }
+    });
+
+    const updateButton = wrapper.find('RaisedButton').first();
+    updateButton.simulate('click');
+    expect(handleProfileUpdate.calledOnce).to.be.true;
+  });
+
+  it('toggles showing the profile update', () => {
+    const handleToggleShowUpdate = sinon.spy();
+    const wrapper = shallowWithContext(<UserSideBarUpdate {...props} />);
+    wrapper.setProps({
+      handleToggleShowUpdate: handleToggleShowUpdate,
+      userDetails: {
+        updatedUser: {},
+        user: {
+          username: 'test',
+          role: {}
+        },
+        validations: {
+          isValid: true
+        }
+      }
+    });
+    const cancelButton = wrapper.find('RaisedButton').last();
+    cancelButton.simulate('click');
+    expect(handleToggleShowUpdate.calledOnce).to.be.true;
+  });
+});
